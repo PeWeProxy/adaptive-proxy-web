ee50223a30a15505b0087a85f9bb7d7b136fca3e
Michal Barla <barla@fiit.stuba.sk>
2009-11-03 23:19:04 +0100

    forgot this one, quite important file

---
+++ app/controllers/logs_controller.rb
@@ -0,0 +1,17 @@
+class LogsController < ApplicationController
+  before_filter :retrieve_logs
+
+  def list
+    @uid = session[:uid]
+  end
+
+  def delete
+    @logs[params[:id].to_i].destroy
+    redirect_to :action => :list
+  end
+
+  private
+  def retrieve_logs
+    @logs = AccessLog.paginate :page => params[:page], :order => 'timestamp DESC'
+  end
+end
