e1441b66a32c7de664c141a16ecf62654e6de0d7
Tomas Kramar <kramar.tomas@gmail.com>
2010-03-02 23:15:37 +0100

    New style.

---
+++ public/javascripts/tutorial.js
@@ -0,0 +1,9 @@
+foldable = function($) {
+  $(document).ready(function() {
+    $('.fold').hide();
+    $('.handle').click(function() {
+      var section_id = '#' + $(this).attr('id') + '-fold';
+      $(section_id).toggle();
+    }).css('cursor', 'pointer');
+  });
+} (jQuery);
