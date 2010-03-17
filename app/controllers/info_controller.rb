ebbf612e372b27b92688aae44876944d10883be4
Tomas Kramar <kramar.tomas@gmail.com>
2010-03-18 00:51:52 +0100

    Update proxy URL to peweproxy, refactor it to controller

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,14 +1,16 @@
 class InfoController < ApplicationController
 
   def index
-    
   end
+
   def user_agent
     @uid = session[:uid]
   end
 
   def settings
-    @uid = session[:uid]    
+    @uid = session[:uid]
+    @proxy_pac = "http://peweproxy.fiit.stuba.sk/proxy.pac"
+    @proxy_url = "http://peweproxy.fiit.stuba.sk:9666"
   end
 
   def privacy
