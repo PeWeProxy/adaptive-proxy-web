efdc314d2c71b5bfd518176fa41235e7f1b55bc4
Tomas Kramar <kramar.tomas@gmail.com>
2010-04-22 22:55:53 +0200

    Log clicks on queries - positive feedback

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -8,4 +8,15 @@ class RecommendationsController < ApplicationController
 
     redirect_to recommendation.recommended_url
   end
+
+  def group
+    group = RecommendationGroup.find_by_id(params[:id])
+    group.clicked = true
+    group.clicked_at = Time.new
+    group.clicked_query = params[:q]
+
+    group.save
+
+    redirect_to("http://www.google.com/search?q=" + CGI::escape(params[:q]) + "#dontrecommend")
+  end
 end
---
+++ app/models/recommendation_group.rb
@@ -0,0 +1,3 @@
+class RecommendationGroup < ActiveRecord::Base
+  set_table_name :bf_recommendation_groups
+end
