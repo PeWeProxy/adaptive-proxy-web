7472a5e11d3aa7c33272e2ee2465ca02a09a8abf
Michal Barla <barla@barla-nb-hp.(none)>
2010-04-28 00:19:51 +0200

    Remove custom disable_web task

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,18 +32,6 @@ namespace :deploy do
   end
 end
 
-task :disable_web, :roles => :web do
-  on_rollback { delete "#{shared_path}/system/maintenance.html" }
-  
-  maintenance = render("./app/views/layouts/maintenance.rhtml", 
-                       :deadline => ENV['UNTIL'],
-                       :reason => ENV['REASON'])
-                       
-  put maintenance, "#{shared_path}/system/maintenance.html", 
-                   :mode => 0644
-end
-
-
 after 'deploy:update_code', 'deploy:symlink_shared'
 after 'deploy', 'deploy:cleanup'
 
