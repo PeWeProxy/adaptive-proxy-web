6b0bbfe88fadfd2576e3b1a3ddc461615366a6d4
Juraj Spusta <spustaj@gmail.com>
2010-11-09 23:58:46 +0100

    Proxy-web to Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdaptiveProxyWeb3::Application.config.secret_token = '0ca6c307bc81045c8c825bd65181a5e36b4257895939913e29bfbda809e0b586940e2cebb7e828d7ba37a820beb2e2c9d33023c6144bb956fcaae4370fa895f1'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_user_registration_with_proxy_session',
-  :secret      => '842cc0902e2cc6990af1dca9dc21d27d7bcccfd932d958967791c62c0f49b75745a8e04483646192962b744d685e849049409d41efefe3fcd281feb318ffe9e8'
-}
+AdaptiveProxyWeb3::Application.config.session_store :cookie_store, :key => '_adaptive-proxy-web-3_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# AdaptiveProxyWeb3::Application.config.session_store :active_record_store
