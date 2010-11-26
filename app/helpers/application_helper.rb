35445f6a0f48e79a9360abf7077eca0b477549e3
Tomas Kramar <kramar.tomas@gmail.com>
2010-11-26 19:13:18 +0100

    Use built-in truncate helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,5 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  MAX_TEXT_LENGTH = 45
-
-  def shorten text
-    if text.length <= MAX_TEXT_LENGTH
-      text
-    else
-      text[0..MAX_TEXT_LENGTH] + '...'
-    end
-  end
-
   def proxy_base_url
     url_for :controller => :info, :action => :index
   end
