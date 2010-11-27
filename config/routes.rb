82d3bd690bc7320a9c63164e76824c6c7eabc4cb
Tomas Kramar <kramar.tomas@gmail.com>
2010-11-27 11:54:05 +0100

    Rework routes to use scopes for sub-uri and locale

--- config/routes.rb
+++ config/routes.rb
@@ -46,18 +46,13 @@ AdaptiveProxyWeb3::Application.routes.draw do
   #     resources :products
   #   end
 
-  filter :locale
+  scope "/proxy" do
+    scope "/:locale" do
+      root :to => "info#index"
+      match ':controller(/:action(/:id(.:format)))'
+      match ':controller(/:action(/:id))'
+    end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-  root :to => "info#index"
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  match ':controller(/:action(/:id(.:format)))'
-  match ':controller(/:action(/:id))'
-  #map.connect ':controller/:action/:id'
-  #map.connect ':controller/:action/:id.:format'
+    root :to => "info#index"
+  end
 end
